cde8cf644d4ae87bd225caea20ba974a72cf20ae
Nishi Anand <nishianand.05@gmail.com>
2021-07-21 13:28:52 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const num_boxes = 32;
+
+// Stateless functional component
+
+const Box = ({color}) => {
+	const style = {
+		width: '180px',
+		height: '180px',
+		display: 'inline-block',
+		backgroundColor: color
+	}
+	
+	return (
+		<div style={style} />
+	);
 }
+
+class App extends Component {
+	
+	constructor(props) {
+		
+		super(props);
+		
+		// String of box's color
+		const boxColors = Array(num_boxes).fill().map(this.getRandomColor, this);
+		
+		this.state = {boxColors};
+		
+		setInterval(() => {
+			
+			//Copy of box's color array
+			const boxColors = this.state.boxColors.slice();
+			
+			const randIndex = Math.floor(Math.random() * boxColors.length);
+			boxColors[randIndex] = this.getRandomColor();
+			
+			this.setState({boxColors});
+		}, 300);
+	}
+	
+	getRandomColor(){
+		let colorIndex = Math.floor(Math.random() * this.props.allColors.length);
+		return this.props.allColors[colorIndex];
+	}
+	
+	render() {
+		
+		const boxes = this.state.boxColors.map((color, index) => (
+			<Box key={index} color={color} />
+		));
+		
+		return (
+			<div className="App">
+				{boxes}
+			</div>
+		);
+	}
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
 
 export default App;
